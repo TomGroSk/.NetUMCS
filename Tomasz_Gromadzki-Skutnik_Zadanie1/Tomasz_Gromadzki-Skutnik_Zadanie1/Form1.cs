0e8049663120bb2c0ffddc591a6947c1a4557a8a
Tomputer <tomek.gromadzki.sk@gmail.com>
2020-04-03 20:06:47 +0200

    Add input validation

--- Tomasz_Gromadzki-Skutnik_Zadanie1/Tomasz_Gromadzki-Skutnik_Zadanie1/Form1.cs
+++ Tomasz_Gromadzki-Skutnik_Zadanie1/Tomasz_Gromadzki-Skutnik_Zadanie1/Form1.cs
@@ -55,37 +55,53 @@ namespace Tomasz_Gromadzki_Skutnik_Zadanie1
         {
             string text1 = textBox1.Text;
             string text2 = textBox2.Text;
-            double m = double.Parse(text1);
-            double z = double.Parse(text2);
             Random random = new Random();
             List<string> globalData = new List<string>();
-            double exactValue = Math.Pow(100, 3) / 3.0;
+            try
+            {
+                double m = double.Parse(text1);
+                double z = double.Parse(text2);
+                double exactValue = Math.Pow(100, 3) / 3.0;
 
-            globalData.Add("Rectangle:");
-            globalData.Add("");
-            globalData.AddRange(Calculation.GetAllValuesDiffFromZ(z, m, exactValue, AreaType.Rectangle));
-            globalData.Add("");
-            globalData.Add("Trapezoid:");
-            globalData.Add("");
-            globalData.AddRange(Calculation.GetAllValuesDiffFromZ(z, m, exactValue, AreaType.Trapezoid));
+                globalData.Add("Rectangle:");
+                globalData.Add("");
+                globalData.AddRange(Calculation.GetAllValuesDiffFromZ(z, m, exactValue, AreaType.Rectangle));
+                globalData.Add("");
+                globalData.Add("Trapezoid:");
+                globalData.Add("");
+                globalData.AddRange(Calculation.GetAllValuesDiffFromZ(z, m, exactValue, AreaType.Trapezoid));
 
-            listBox1.DataSource = globalData;
+                listBox1.DataSource = globalData;
+            }
+            catch (Exception fe)
+            {
+                globalData.Add("Error! Incorrect data! Please enter the correct one.");
+                listBox1.DataSource = globalData;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             string text = textBox3.Text;
-            double z = double.Parse(text);
-            Random random = new Random();
-            double exactValue = Math.Pow(100, 4) / 4.0;
             List<string> globalData = new List<string>();
+            try
+            {
+                double z = double.Parse(text);
+                Random random = new Random();
+                double exactValue = Math.Pow(100, 4) / 4.0;
 
-            globalData.Add("Rectangle: ");
-            globalData.Add(Calculation.FindNDiffFromZ(z, exactValue, AreaType.Rectangle));
-            globalData.Add("Trapezoid: ");
-            globalData.Add(Calculation.FindNDiffFromZ(z, exactValue, AreaType.Trapezoid));
-            globalData.Add("");
-            listBox2.DataSource = globalData;
+                globalData.Add("Rectangle: ");
+                globalData.Add(Calculation.FindNDiffFromZ(z, exactValue, AreaType.Rectangle));
+                globalData.Add("Trapezoid: ");
+                globalData.Add(Calculation.FindNDiffFromZ(z, exactValue, AreaType.Trapezoid));
+                globalData.Add("");
+                listBox2.DataSource = globalData;
+            }
+            catch (Exception fe)
+            {
+                globalData.Add("Error! Incorrect data! Please enter the correct one.");
+                listBox2.DataSource = globalData;
+            }
 
         }
 
@@ -95,24 +111,32 @@ namespace Tomasz_Gromadzki_Skutnik_Zadanie1
             string text2 = textBox5.Text;
             double x1 = double.Parse(text1);
             double x2 = double.Parse(text2);
-            Random random = new Random();
-            int n = random.Next(10, 1000000);
-            double tempResult = 0;
             List<string> finalData = new List<string>();
-
-            SingleCount singleCountRectangle = new SingleCount(x1, x2, n, AreaType.Rectangle, 0, 1);
-            SingleCount singleCountTrapezoid = new SingleCount(x1, x2, n, AreaType.Trapezoid, 0, 1);
-            Global globalRectangleValue = Calculation.CalculateIntegralFull(singleCountRectangle, x => x * x);
-            Global globalTrapezoidValue = Calculation.CalculateIntegralFull(singleCountTrapezoid, x => x * x);
-
-            for (int i = 0; i < n; i++)
+            Random random = new Random();
+            try
             {
-                tempResult += Math.Pow((globalRectangleValue.SingleCountsList[i].Area - globalTrapezoidValue.SingleCountsList[i].Area), 2);
-            }
+                int n = random.Next(10, 1000000);
+                double tempResult = 0;
 
-            finalData.Add("Błąd średniokwadratowy dla n równego - " + n.ToString());
-            finalData.Add((tempResult / n).ToString());
-            listBox3.DataSource = finalData;
+                SingleCount singleCountRectangle = new SingleCount(x1, x2, n, AreaType.Rectangle, 0, 1);
+                SingleCount singleCountTrapezoid = new SingleCount(x1, x2, n, AreaType.Trapezoid, 0, 1);
+                Global globalRectangleValue = Calculation.CalculateIntegralFull(singleCountRectangle, x => x * x);
+                Global globalTrapezoidValue = Calculation.CalculateIntegralFull(singleCountTrapezoid, x => x * x);
+
+                for (int i = 0; i < n; i++)
+                {
+                    tempResult += Math.Pow((globalRectangleValue.SingleCountsList[i].Area - globalTrapezoidValue.SingleCountsList[i].Area), 2);
+                }
+
+                finalData.Add("Błąd średniokwadratowy dla n równego - " + n.ToString());
+                finalData.Add((tempResult / n).ToString());
+                listBox3.DataSource = finalData;
+            }
+            catch (Exception fe)
+            {
+                finalData.Add("Error! Incorrect data! Please enter the correct one.");
+                listBox3.DataSource = finalData;
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -121,95 +145,127 @@ namespace Tomasz_Gromadzki_Skutnik_Zadanie1
             string text2 = textBox7.Text;
             string text3 = textBox8.Text;
             string text4 = textBox9.Text;
-
-            double z = double.Parse(text3);
-            int n = (int)Math.Pow(10, int.Parse(text4));
-            double x1 = double.Parse(text2);
-            double x2 = double.Parse(text1);
-
             List<string> globalData = new List<string>();
-            globalData.Add("Rectangle:");
-            globalData.Add(Calculation.FindX1X2DivByZ(x1, x2, n, z, AreaType.Rectangle));
-            globalData.Add("Trapezoid:");
-            globalData.Add(Calculation.FindX1X2DivByZ(x1, x2, n, z, AreaType.Trapezoid));
 
-            listBox4.DataSource = globalData;
+            try
+            {
+                double z = double.Parse(text3);
+                int n = (int)Math.Pow(10, int.Parse(text4));
+                double x1 = double.Parse(text2);
+                double x2 = double.Parse(text1);
+                globalData.Add("Rectangle:");
+                globalData.Add(Calculation.FindX1X2DivByZ(x1, x2, n, z, AreaType.Rectangle));
+                globalData.Add("Trapezoid:");
+                globalData.Add(Calculation.FindX1X2DivByZ(x1, x2, n, z, AreaType.Trapezoid));
+                listBox4.DataSource = globalData;
+            }
+            catch (Exception fe)
+            {
+                globalData.Add("Error! Incorrect data! Please enter the correct one.");
+                listBox4.DataSource = globalData;
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             string text1 = textBox11.Text;
-            int n = (int)Math.Pow(10, int.Parse(text1));
-
             List<string> globalRectangle = new List<string>();
-            List<string> globalTrapezoid = new List<string>();
-            globalRectangle.Add("Rectangle:");
-            globalTrapezoid.Add("Trapezoid:");
+            List<string> globalTrapezoid = new List<string>();   
+            try
+            {
+                int n = (int)Math.Pow(10, int.Parse(text1));
+                globalRectangle.Add("Rectangle:");
+                globalTrapezoid.Add("Trapezoid:");
 
-            globalRectangle.Add(Calculation.FindX1X2ForFunctionValuesEquality(n, AreaType.Rectangle));
-            globalTrapezoid.Add(Calculation.FindX1X2ForFunctionValuesEquality(n, AreaType.Trapezoid));
+                globalRectangle.Add(Calculation.FindX1X2ForFunctionValuesEquality(n, AreaType.Rectangle));
+                globalTrapezoid.Add(Calculation.FindX1X2ForFunctionValuesEquality(n, AreaType.Trapezoid));
 
-            globalRectangle.AddRange(globalTrapezoid);
-            listBox5.DataSource = globalRectangle;
-
+                globalRectangle.AddRange(globalTrapezoid);
+                listBox5.DataSource = globalRectangle;
+            }
+            catch (Exception fe)
+            {
+                globalRectangle.Add("Error! Incorrect data! Please enter the correct one.");
+                listBox5.DataSource = globalRectangle;
+            }
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             string text1 = textBox10.Text;
             string text2 = textBox12.Text;
-            int m = int.Parse(text1);
-            int n = (int)Math.Pow(10, int.Parse(text2));
             Random random = new Random();
             List<string> globalData = new List<string>();
+            try
+            {
+                int m = int.Parse(text1);
+                int n = (int)Math.Pow(10, int.Parse(text2));
+                globalData.Add("Rectangle:");
+                globalData.Add(Calculation.FindMinDiff(m, n, AreaType.Rectangle));
+                globalData.Add("");
+                globalData.Add("Trapezoid:");
+                globalData.Add(Calculation.FindMinDiff(m, n, AreaType.Trapezoid));
 
-            globalData.Add("Rectangle:");
-            globalData.Add(Calculation.FindMinDiff(m, n, AreaType.Rectangle));
-            globalData.Add("");
-            globalData.Add("Trapezoid:");
-            globalData.Add(Calculation.FindMinDiff(m, n, AreaType.Trapezoid));
-
-            listBox6.DataSource = globalData;
-
+                listBox6.DataSource = globalData;
+            }
+            catch (Exception fe)
+            {
+                globalData.Add("Error! Incorrect data! Please enter the correct one.");
+                listBox6.DataSource = globalData;
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            double x1 = double.Parse(textBox16.Text);
-            double x2 = double.Parse(textBox15.Text);
-            double z = double.Parse(textBox13.Text);
             List<string> globalData = new List<string>();
-
-            if (x1 == x2)
+            try
             {
-                globalData.Add("NaN");
-                listBox7.DataSource = globalData;
-                return;
-            }
+                double x1 = double.Parse(textBox16.Text);
+                double x2 = double.Parse(textBox15.Text);
+                double z = double.Parse(textBox13.Text);
 
-            globalData.Add("Rectangle:");
-            globalData.Add(Calculation.FindFirstNDivideByZ(x1, x2, z, AreaType.Rectangle));
-            globalData.Add("Trapezoid:");
-            globalData.Add(Calculation.FindFirstNDivideByZ(x1, x2, z, AreaType.Trapezoid));
-            listBox7.DataSource = globalData;
+                if (x1 == x2)
+                {
+                    globalData.Add("NaN");
+                    listBox7.DataSource = globalData;
+                    return;
+                }
+
+                globalData.Add("Rectangle:");
+                globalData.Add(Calculation.FindFirstNDivideByZ(x1, x2, z, AreaType.Rectangle));
+                globalData.Add("Trapezoid:");
+                globalData.Add(Calculation.FindFirstNDivideByZ(x1, x2, z, AreaType.Trapezoid));
+                listBox7.DataSource = globalData;
+            }
+            catch (Exception fe)
+            {
+                globalData.Add("Error! Incorrect data! Please enter the correct one.");
+                listBox7.DataSource = globalData;
+            }
 
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            double z = double.Parse(textBox14.Text);
-            double x2 = Math.PI / 2;
-            double exactValue = Math.Sin(x2);
-            List<string> globalData = new List<string>
+            List<string> globalData = new List<string>();
+            try
             {
-                "Rectangle: ",
-                Calculation.FindFirstN(x2, z, exactValue, AreaType.Rectangle),
+                double z = double.Parse(textBox14.Text);
+                double x2 = Math.PI / 2;
+                double exactValue = Math.Sin(x2);
 
-                "Trapezoid: ",
-                Calculation.FindFirstN(x2, z, exactValue, AreaType.Trapezoid)
-            };
+                globalData.Add("Rectangle:");
+                globalData.Add(Calculation.FindFirstN(x2, z, exactValue, AreaType.Rectangle));
+                globalData.Add("Trapezoid:");
+                globalData.Add(Calculation.FindFirstN(x2, z, exactValue, AreaType.Trapezoid));
 
-            listBox8.DataSource = globalData;
+                listBox8.DataSource = globalData;
+            }
+            catch (Exception fe)
+            {
+                globalData.Add("Error! Incorrect data! Please enter the correct one.");
+                listBox8.DataSource = globalData;
+            }
         }
     }
 }
